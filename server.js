bc8a324d5a53a971a36281fb391fd0d238d2a9bf
DIVYA525-VAD <68990221+DIVYA525-VAD@users.noreply.github.com>
2023-07-07 09:29:57 +0530

    some minor changes

--- server.js
+++ server.js
@@ -1,33 +1,28 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3001;
 
-app.use(cors(
-  {
-    origin:'https://splendid-gumdrop-3a6b2a.netlify.app/',
-    methods:["GET"]
-  }
-));
+app.use(cors());
 
-app.get('/api/photo-gallery-feed-page/page/:page', async (req, res) => {
+app.get("/api/photo-gallery-feed-page/page/:page", async (req, res) => {
   const { page } = req.params;
   try {
     const response = await axios.get(
       `https://englishapi.pinkvilla.com/app-api/v1/photo-gallery-feed-page/page/${page}`,
       {
         headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
         },
       }
     );
     res.json(response.data);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Failed to fetch data' });
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Failed to fetch data" });
   }
 });
 
